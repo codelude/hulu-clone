840fae512d2937ef0190d6487912ec602f99e653
Shawaz Sharif <shawaz@users.noreply.github.com>
2022-10-03 15:47:59 +0530

    inital commit

---
+++ build/precache-manifest.a99654a4a6353a20616b7f4dd9a42339.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df0e6c69077af4269a26d70a4c9227b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63a61e33cff527df5f77",
+    "url": "/static/css/main.8b2ee4b4.chunk.css"
+  },
+  {
+    "revision": "ed9a71612035586d34af",
+    "url": "/static/js/2.b3bb961f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b3bb961f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63a61e33cff527df5f77",
+    "url": "/static/js/main.6577796c.chunk.js"
+  },
+  {
+    "revision": "e23f96d817477a4cb65a",
+    "url": "/static/js/runtime-main.268a3519.js"
+  }
+]);
